cc4279029aa65986c19797c23439b08f441e8508
ajnyga <ajnyga@gmail.com>
2020-03-24 11:00:05 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Open Graph
+Exposes OJS and OPS metadata using the Open Graph protocol (https://ogp.me/). The metadata is used when published submissions are shared in Facebook and LinkedIn.
+
+This plugin is included in compatible OJS and OPS releases; please do not install it manually.
---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @defgroup plugins_generic_openGraph OpenGraph plugin
+ */
+
+/**
+ * @file plugins/generic/openGraph/index.php
+ *
+ * Copyright (c) 2014-2020 Simon Fraser University
+ * Copyright (c) 2003-2020 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @ingroup plugins_generic_openGraph
+ * @brief Wrapper for OpenGraph plugin.
+ *
+ */
+
+require_once('OpenGraphPlugin.inc.php');
+
+return new OpenGraphPlugin();
+
+
